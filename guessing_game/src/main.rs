410922e12df870df191fd3736d0d59a90710dd60
Colin Fletcher <cfletcher1856@gmail.com>
2024-10-05 10:53:16 -0500

    Adding better error handling

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,22 +6,25 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
 
     println!("The secret number is: {}", secret_number);
 
     loop {
         println!("Please input your guess.");
 
-        let mut guess = String::new();
+        let mut guess: String = String::new();
 
         io::stdin()
             .read_line(&mut guess)
-            .expect("Faile to read line");
+            .expect("Failed to read line");
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("{}", "Please input a number!".yellow());
+                continue;
+            }
         };
 
         println!("You guessed: {}", guess);
